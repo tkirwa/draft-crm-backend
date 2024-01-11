5271bb933be2e7d4ee489dc6fdfa7aa85c429a9b
Tonny Kirwa <tonnykirwa@yahoo.com>
2024-01-11 15:40:43 +0300

    fixed sms sending

--- controllers/sendSmsController.js
+++ controllers/sendSmsController.js
@@ -1,35 +1,33 @@
 // Load environment variables from .env file
-require('dotenv').config();
+require("dotenv").config();
 
 const credentials = {
-    apiKey: process.env.AFRICAN_TALKING_API_KEY,
-    username: process.env.AFRICAN_TALKING_USERNAME,
-}
+  apiKey: process.env.AFRICAN_TALKING_API_KEY,
+  username: process.env.AFRICAN_TALKING_USERNAME,
+};
 
-const Africastalking = require('africastalking')(credentials);
+const Africastalking = require("africastalking")(credentials);
 
 // Initialize a service e.g. SMS
 const sms = Africastalking.SMS;
 
 exports.sendSMS = async (req, res) => {
-
+  try {
     const { smsPhone, smsMessage } = req.body;
 
+    const formattedPhone = `+${smsPhone}`;
 
-    try {
-        const formattedPhone = `+${smsPhone}`;
+    // Send SMS using Africa's Talking
+    const response = await sms.send({
+      to: [formattedPhone], // Ensure that smsPhone is a string in the format '+2547XXXXXXXX'
+      message: smsMessage,
+      enqueue: true, // Set to true to enable queuing (optional)
+    });
 
-        // Send SMS using Africa's Talking
-        const response = await sms.send({
-            to: [formattedPhone], // Ensure that smsPhone is a string in the format '+2547XXXXXXXX'
-            message: smsMessage,
-            enqueue: true // Set to true to enable queuing (optional)
-        });
-
-        console.log(response);
-        return { sent: 1, message: response };
-    } catch (error) {
-        console.error(error);
-        return { sent: 0, message: error.message };
-    }
+    console.log(response);
+    res.json({ sent: 1, message: response });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ sent: 0, message: error.message });
+  }
 };
