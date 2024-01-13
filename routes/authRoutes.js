ee41ec2ad866c6bc23f3f40ca29996f64bead1a8
Tonny Kirwa <tonnykirwa@yahoo.com>
2024-01-13 23:55:00 +0300

    fixed routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,10 +3,10 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 const { validateResetPasswordInput } = require("../middlewares/authMiddleware");
-const { validateUser } = require("../middlewares/userMiddleware");
+const { validateUser, checkIfUsersExist } = require("../middlewares/userMiddleware");
 
 router.post("/login", authController.login);
-router.post("/signup", validateUser, authController.register);
+router.post("/signup", [validateUser], authController.register);
 // router.post('/reset-password', validateResetPasswordInput, authController.resetPassword);
 router.put('/reset-password', authController.resetPassword);
 
--- x_build_app.sh
+++ x_build_app.sh
@@ -2,4 +2,4 @@
 
 mongodump --host 0.0.0.0 --port 27017 --db clarek_crm_db --out .
 
-docker build -t draft-crm-backend_api_v3 .
+docker build -t draft-crm-backend_api_v4 .
